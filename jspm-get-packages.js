ccd25bebc32e416c88a068f4d0100b0a084575d7
Sasha <sasha.antonchikov@gmail.com>
2020-04-27 19:36:18 +0300

    use via arg and result into output

--- jspm-get-packages.js
+++ jspm-get-packages.js
@@ -1,61 +1,74 @@
 const jq = require('node-jq')
 const fs = require('fs')
 
-const jsonPath = 'package-with-jspm.json'
+const myArgs = process.argv.slice(2);
+
+// const jsonPath = 'package-with-jspm.json'
+const jsonPath = myArgs[0]
+if(jsonPath.length === 0) {
+    console.error("Please enter package.json with 'jspm.dependencies' section")
+    return 1;
+}
+
+let dir_output = "output"
+if (!fs.existsSync(dir_output)){
+    fs.mkdirSync(dir_output);
+}
 
 // const jsonPath = '.jspm.dependencies[] | select( contains("jspm"))'
 
 let step_number = 0
-async function stepJQ(filter, jsonPathOrData, options) {
-    let f_name = `result_${step_number}.json`
-    step_number++
+async function stepJQ(filter, jsonPathOrData, options, resultPath) {
+    // let f_name = `result_${step_number}.json`
+    // step_number++
 
     // options = {raw: true} - print without quotes!!!!!
     let result = await jq.run(filter, jsonPathOrData, options);  
     
-    fs.writeFileSync(f_name, result, 'utf8')
-    return f_name
+    fs.writeFileSync(resultPath, result, 'utf8')
+    // return f_name
 }
 
 (async() => {
     
     // STEP1 get dependencies
+    let resultPath1 = `${dir_output}/1.jspm.dependencies.json`
     let filter1 = '.jspm.dependencies'
-    let f_name1 = await stepJQ(filter1, jsonPath, {output: 'pretty'})
-    let data = fs.readFileSync(f_name1, 'utf8');
+    await stepJQ(filter1, jsonPath, {output: 'pretty'}, resultPath1)
+    
+    let data = fs.readFileSync(resultPath1, 'utf8');
     console.log(data)
 
     // STEP2 remove jspm and systemjs packs
+    let resultPath2 = `${dir_output}/2.jspm.dependencies.no-jspm.no-systemjs.json`
     // let filter2 = '[to_entries[] | select(.value | contains("jspm") ==false and contains("systemjs") ==false ) ]'
     // let filter2 = '[to_entries[] | select(.value | contains("jspm") ==false and contains("systemjs") ==false )  | {(.key): .value} ] 
     let filter2 = '[to_entries[] | select(.value | contains("jspm") ==false and contains("systemjs") ==false )  | {(.key): .value}] | add '
-    let f_name2 = await stepJQ(filter2, f_name1, {output: 'pretty'})
-    data = fs.readFileSync(f_name2, 'utf8');
+    await stepJQ(filter2, resultPath1, {output: 'pretty'}, resultPath2)
+    data = fs.readFileSync(resultPath2, 'utf8');
     console.log(data)
 
     // STEP3 filter npm packs and prepare install script
+    let resultPath3 = `${dir_output}/3.npm-install.sh`
     // let filter3 = '[to_entries[] | select(.value | contains("jspm") ==false and contains("systemjs") ==false )  | {(.key): .value}] | add [] | "npm i --save \(sub( "github:|npm:"; ""))"'
     // let filter3 = '.[] | "npm i --save \\(sub( "github:|npm:"; "")) &&"'
-    let filter3 = '.[] | select(contains("npm")) | "echo \\"\\(.)\\" &&\n npm i --save \\(.) &&"'
-    let f_name3 = await stepJQ(filter3, f_name2, {raw: true})
-    data = fs.readFileSync(f_name3, 'utf8');
+    let filter3 = '.[] | select(contains("npm")) | "echo \\"npm i --save \\(.)\\" &&\n npm i --save \\(.) &&"'
+    await stepJQ(filter3, resultPath2, {raw: true}, resultPath3)
+    data = fs.readFileSync(resultPath3, 'utf8');
     console.log(data)
-    fs.appendFileSync(f_name3, '\n', 'utf8')
-    fs.appendFileSync(f_name3, 'echo "ALL RIGHT😉"', 'utf8')
+    fs.appendFileSync(resultPath3, 'echo "\nALL RIGHT😉"', 'utf8')
 
     // STEP4 filter github packs and prepare install script
+    let resultPath4 = `${dir_output}/3.github-install.sh`
     // let filter4 = '[to_entries[] | select(.value | contains("jspm") ==false and contains("systemjs") ==false )  | {(.key): .value}] | add [] | "npm i --save \(sub( "github:|npm:"; ""))"'
     // let filter4 = '.[] | "npm i --save \\(sub( "github:|npm:"; "")) &&"'
     // let filter4 = '.[] | select(contains("github")) | "echo \\"\\(.)\\" &&\n npm i --save \\(sub("@\\\\^|@"; "#v")) &&"'
-    let filter4 = '.[] | select(contains("github")) | sub("@\\\\^|@"; "#v") | sub("#vmaster";"") | "echo \\"\\(.)\\" &&\n npm i --save \\(.) &&"'
+    let filter4 = '.[] | select(contains("github")) | sub("@\\\\^|@"; "#v") | sub("#vmaster";"") | "echo \\"npm i --save \\(.)\\" &&\n npm i --save \\(.) &&"'
     
-    let f_name4 = await stepJQ(filter4, f_name2, {raw: true})
-    data = fs.readFileSync(f_name4, 'utf8');
+    await stepJQ(filter4, resultPath2, {raw: true}, resultPath4)
+    data = fs.readFileSync(resultPath4, 'utf8');
     console.log(data)
-    fs.appendFileSync(f_name4, '\n', 'utf8')
-    fs.appendFileSync(f_name4, 'echo "ALL RIGHT😉"', 'utf8')
-
-
+    fs.appendFileSync(resultPath4, 'echo "\nALL RIGHT😉"', 'utf8')
 
 })();
 
---
+++ output/3.github-install.sh
@@ -0,0 +1,17 @@
+echo "npm i --save github:Niskigvan/mdPickers" &&
+ npm i --save github:Niskigvan/mdPickers &&
+echo "npm i --save github:SheetJS/sheetjs#v0.14.0" &&
+ npm i --save github:SheetJS/sheetjs#v0.14.0 &&
+echo "npm i --save github:alirezamirian/angular-material-lightbox#v0.0.11" &&
+ npm i --save github:alirezamirian/angular-material-lightbox#v0.0.11 &&
+echo "npm i --save github:angular-ui-tree/angular-ui-tree#v2.22.6" &&
+ npm i --save github:angular-ui-tree/angular-ui-tree#v2.22.6 &&
+echo "npm i --save github:Niskigvan/mdPickers" &&
+ npm i --save github:Niskigvan/mdPickers &&
+echo "npm i --save github:Niskigvan/pouchdb-find" &&
+ npm i --save github:Niskigvan/pouchdb-find &&
+echo "npm i --save github:protobi/js-xlsx#v0.8.6" &&
+ npm i --save github:protobi/js-xlsx#v0.8.6 &&
+echo "npm i --save github:sockjs/sockjs-client#v1.1.4" &&
+ npm i --save github:sockjs/sockjs-client#v1.1.4 &&echo "
+ALL RIGHT😉"
